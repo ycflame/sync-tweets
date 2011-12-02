0c3b2da1c1a9868e3320b9ad7f82aba7669409d7
Chao Yang <yangchao.cs@gmail.com>
2011-12-02 12:54:08 +0800

    add OAuth support

--- twitter.py
+++ twitter.py
@@ -12,6 +12,7 @@ import re
 import htmlentitydefs
 import time
 import urllib,urllib2,Cookie
+import oauth2 as oauth
 from google.appengine.api import urlfetch
 from google.appengine.ext import db
 
@@ -59,102 +60,19 @@ def getLatest():
     else:
         return ""
 
-def send_sina_msgs(username,password,msg):
-	auth=base64.b64encode(username+":"+password)
-	auth='Basic '+auth
+def send_fanfou_msgs(msg):
+	consumer = oauth.Consumer(key="your_consumer_key", secret="your_consumer_secret")
+	token = oauth.Token(key="your_access_token", secret="your_access_secret")
+	post_url = "http://api.fanfou.com/statuses/update.json"
+
+	client = oauth.Client(consumer, token)
 	msg=unescape(msg)
 	form_fields = {
-			"status": msg,
+			"status": msg
 			}
 	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.t.sina.com.cn/statuses/update.xml?source=App_Key",
-			payload=form_data,
-			method=urlfetch.POST,
-			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
-def send_sina_web_msgs(username,password,msg):
-        # send sina msgs. use sina username, password.
-        # the msgs parameter is a message list, not a single string.       
-        result = urlfetch.fetch(url="https://login.sina.com.cn/sso/login.php?username=%s&password=%s&returntype=TEXT"%(username,password))
-        cookie = Cookie.SimpleCookie(result.headers.get('set-cookie', ''))
-        msg=unescape(msg)
-        form_fields = {
-          "content": msg,          
-        }
-        form_data = urllib.urlencode(form_fields)
-
-        result = urlfetch.fetch(url="http://t.sina.com.cn/mblog/publish.php",
-                            payload=form_data,
-                            method=urlfetch.POST,
-                            headers={'Referer':'http://t.sina.com.cn','Cookie' : make_cookie_header(cookie)})
-        #print ""
-        #print result.content
-        
-def send_163_msgs(username,password,msg):
-        # send sina msgs. use sina username, password.
-        # the msgs parameter is a message list, not a single string.       
-        result = urlfetch.fetch(url="https://reg.163.com/logins.jsp?username=%s&password=%s&product=t&type=1"%(username,password))
-        cookie = Cookie.SimpleCookie(result.headers.get('set-cookie', ''))
-        msg=unescape(msg)
-        form_fields = {
-          "status": msg,          
-        }
-        form_data = urllib.urlencode(form_fields)
-
-        result = urlfetch.fetch(url="http://t.163.com/statuses/update.do",
-                            payload=form_data,
-                            method=urlfetch.POST,
-                            headers={'Referer':'http://t.163.com','Cookie' : make_cookie_header(cookie)})
-        #print ""
-        #print result.content
-        
-def send_sohu_msgs(username,password,msg):
-	auth=base64.b64encode(username+":"+password)
-	auth='Basic '+auth
-	msg=unescape(msg)
-	form_fields = {
-			"status": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.t.sohu.com/statuses/update.xml",
-			payload=form_data,
-			method=urlfetch.POST,
-			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
-def send_fanfou_msgs(username,password,msg):
-	auth=base64.b64encode(username+":"+password)
-	auth='Basic '+auth
-	msg=unescape(msg)
-	form_fields = {
-			"status": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.fanfou.com/statuses/update.xml",
-			payload=form_data,
-			method=urlfetch.POST,
-			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
+	client.request(post_url, "POST", form_data)
+	
 def send_digu_msgs(username,password,msg):
 	auth=base64.b64encode(username+":"+password)
 	auth='Basic '+auth
@@ -167,125 +85,6 @@ def send_digu_msgs(username,password,msg):
 			payload=form_data,
 			method=urlfetch.POST,
 			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-    
-def send_9911_msgs(username,password,msg):
-	auth=base64.b64encode(username+":"+password)
-	auth='Basic '+auth
-	msg=unescape(msg)
-	form_fields = {
-			"status": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.9911.com/statuses/update.xml",
-			payload=form_data,
-			method=urlfetch.POST,
-			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
-def send_zuosa_msgs(username,password,msg):
-	auth=base64.b64encode(username+":"+password)
-	auth='Basic '+auth
-	msg=unescape(msg)
-	form_fields = {
-			"status": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.zuosa.com/statuses/update.xml",
-			payload=form_data,
-			method=urlfetch.POST,
-			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
-def send_renjian_msgs(username,password,msg):
-	auth=base64.b64encode(username+":"+password)
-	auth='Basic '+auth
-	msg=unescape(msg)
-	form_fields = {
-			"text": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.renjian.com/statuses/update.xml",
-			payload=form_data,
-			method=urlfetch.POST,
-			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
-def send_follow5_msgs(username,password,msg):
-	auth=base64.b64encode(username+":"+password)
-	auth='Basic '+auth
-	msg=unescape(msg)
-	form_fields = {
-			"status": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.follow5.com/api/statuses/update.xml?api_key=9E76EE7693D280446FB6CA4A30754ED8",
-			payload=form_data,
-			method=urlfetch.POST,
-			headers={'Authorization':auth}
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
-def send_pingfm_msgs(api_key,user_app_key,msg):
-	msg=unescape(msg)
-	form_fields = {
-			"api_key": api_key,
-			"user_app_key": user_app_key,
-			"post_method": "default",
-			"body": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://api.ping.fm/v1/user.post",
-			payload=form_data,
-			method=urlfetch.POST
-			)
-	if result.status_code == 200:
-		bk=result.content
-		if bk.find("true"):
-			return True
-	else:
-		return False
-
-def send_hellotxt_msgs(user_key,app_key,msg):
-	msg=unescape(msg)
-	form_fields = {
-			"user_key": user_key,
-			"app_key": app_key,
-			"body": msg,
-			}
-	form_data = urllib.urlencode(form_fields)
-	result = urlfetch.fetch(url="http://hellotxt.com/api/v1/method/user.post",
-			payload=form_data,
-			method=urlfetch.POST
 			)
 	if result.status_code == 200:
 		bk=result.content
@@ -316,27 +115,17 @@ def parseTwitter(twitter_id,since_id="",):
 			urls = re.findall("http\S+",text) 
 			# if find some 
 			if len(urls)!=0: 
-			for url in urls: 
-				# construct the new link 
-				suburl = re.sub("t.co","233.im/tco.php",url) 
-				content = urllib2.urlopen(suburl).read() 
-				# get the origin link 
-				result = re.search("http\S+",content) 
-				# substitute the t.co link in the tweets 
-				text = re.sub(url,result.group(0),text)
-
+				for url in urls: 
+					# construct the new link 
+					suburl = re.sub("t.co","233.im/tco.php",url) 
+					content = urllib2.urlopen(suburl).read() 
+					# get the origin link 
+					result = re.search("http\S+",content) 
+					# substitute the t.co link in the tweets 
+					text = re.sub(url,result.group(0),text)
 # You MUST modify your username and password here ##############################################
-			#ret = send_sina_msgs("username@sina.com","password",text)
 			ret = send_digu_msgs("username","password",text)
-			ret = send_fanfou_msgs("username","password",text)
-			#ret = send_163_msgs("username@163.com","password",text)
-			#ret = send_sohu_msgs("username@sohu.com","password",text)
-			#ret = send_9911_msgs("username","password",text)
-			#ret = send_zuosa_msgs("username","password",text)
-			#ret = send_renjian_msgs("username","password",text)
-			#ret = send_follow5_msgs("username","password",text)
-			#ret = send_pingfm_msgs("api_key","user_app_key",text)
-			#ret = send_hellotxt_msgs("user_key","app_key",text)
+			send_fanfou_msgs(text)
 			msg=Twitter()
 			msg.id=id
 			msg.put()
@@ -348,4 +137,4 @@ def parseTwitter(twitter_id,since_id="",):
 print ""
 latest=getLatest() 
 # You MUST modify your twitter username  here ##################################################
-parseTwitter(twitter_id="beyondchaos",since_id=latest)
+parseTwitter(twitter_id="username",since_id=latest)
